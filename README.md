87da229423908b59e621f9164106553976e6745a
uptheirons78 <bono.mauro@gmail.com>
2019-08-23 14:40:01 +0200

    routing and pages

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 2. i react-router-dom and react-icons;
 3. add \_redirects file for Netlify issues;
 4. add images folder and data.js inside src;
+5. create pages folder and files Home, Rooms, SingleRoom and Error;
+6. wrap the app in the BrowserRouter (index.js);
+7. create routes with Route and Switch in App.js;
--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-// import './App.css';
+import React, { Fragment } from 'react';
+import './App.css';
+import Home from './pages/Home';
+import Error from './pages/Error';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
 	return (
-		<div className='App'>
-			<h1>Resort</h1>
-		</div>
+		<Fragment>
+			<Switch>
+				<Route path='/' exact component={Home} />
+				<Route path='/rooms/' exact component={Rooms} />
+				<Route path='/rooms/:slug' exact component={SingleRoom} />
+				<Route component={Error} />
+			</Switch>
+		</Fragment>
 	);
 }
 
